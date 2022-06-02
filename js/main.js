346c80070e231af2607388df67bb0bfa7ef67563
summer4im <106740076+summer4im@users.noreply.github.com>
2022-06-02 21:49:38 +0900

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -18,6 +18,10 @@ $(function () {
         dots: true,
     });
 
+    $(".main_slider figure").eq(1).addClass("on");
+    $(".main_slider").on("afterChange", function (e, s, c) {
+        $(".main_slider figure").eq(c + 1).addClass("on").siblings().removeClass("on");
+    });
 
 })
 
